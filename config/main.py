a546727db1f36473226dc61b23f40b84a218e3e4
Firas Said Midani <firasmidani@gmail.com>
2019-06-07 10:29:45 -0500

    file init

---
+++ config/main.py
@@ -0,0 +1,11 @@
+#!/bin/user/env python
+
+# configuation file
+
+# PURPOSE: store defaults for parameters called by internal functions. 
+# New users who have different defaults would benefit from not calling alternative parameters at every use. 
+
+measurement = 'Optical Density'
+od_wavelength = 620
+plate_size = (8,12)
+time_units = 'seconds'
