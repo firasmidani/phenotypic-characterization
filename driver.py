d65e907bad174064be612323da64b34b17db0f28
Firas Said Midani <firasmidani@gmail.com>
2019-09-12 14:54:09 -0500

    streamlining with functions

--- driver.py
+++ driver.py
@@ -104,12 +104,12 @@ DIRECTORY['MAPPING'] = '%s/mapping' % DIRECTORY['PARENT']
 
 FILES['META'] = '%s/meta.txt' % DIRECTORY['MAPPING']
 
-print('\n')
 checkDirectoryExists(DIRECTORY['PARENT'],'Input directory',verbose=True,sys_exit=True)
 checkDirectoryExists(DIRECTORY['DATA'],'Data directory',verbose=True)
 checkDirectoryNotEmpty(DIRECTORY['DATA'],'Data directory',verbose)
 
 # initialize a list of all input files
+print 'READING & ASSEMBLING DATA'
 list_data = sorted(os.listdir(DIRECTORY['DATA']));
 DATA = readPlateReaderFolder(folderpath=DIRECTORY['DATA'],save=True,save_dirname=DIRECTORY['DERIVED'])
 
@@ -117,30 +117,60 @@ DATA = readPlateReaderFolder(folderpath=DIRECTORY['DATA'],save=True,save_dirname
 BOOL_meta = os.path.exists(FILES['META'])
 #check format of mapping file. Must have PLATE_ID.
 
-# read meta.txt or initialize
-if BOOL_meta:
-    df_meta = pd.read_csv(FILES['META'],sep='\t',header=0,index_col=None);
-    df_meta_plates = df_meta.Plate_ID.values;
+# # read meta.txt or initialize
+# if not BOOL_meta:
+#     df_meta = None
+#     df_meta_plates = [];
+# else:
+#     df_meta = pd.read_csv(FILES['META'],sep='\t',header=0,index_col=None);
 
-    print 'Meta-Data is'
-    print df_meta
+# if 'Plate_ID' not in df_meta.keys():
+#     df_meta_plates = [];
+# else:
+#     df_meta_plates = df_meta.Plate_ID.values;
 
-for filename in list_data:
-  
-    filebase = os.path.spliext(filename)[0]
-    mapping_path = '%s/%s.txt' % (DIRECTORY['MAPPING'],filebase)
+#     print 
+#     print 'Meta-Data is'
+#     print df_meta
+#     print 
+
+def checkMetaTxt(path):
+
+    BOOL_meta = os.path.exists(FILES['META'])
+
+    if not BOOL_meta:
+        df_meta = None
+        df_meta_plates = [];
+    else:
+        df_meta = pd.read_csv(FILES['META'],sep='\t',header=0,index_col=None);
+
+    if 'Plate_ID' not in df_meta.keys():
+        df_meta_plates = [];
+    else:
+        df_meta_plates = df_meta.Plate_ID.values;
+
+        print 
+        print 'Meta-Data is'
+        print df_meta
+        print 
+
+    return df_meta,df_meta_plates
+
+def smartMapping(filebase,DIRECTORY,DATA,df_meta,df_meta_plates):
+
+    mapping_path = '%s/%s.txt' % (DIRECTORY['MAPPING'],filebase);
 
     data = DATA[filebase];
     well_ids = data.index;
 
     if os.path.exists(mapping_path):
-   
-        print 'Reading %s' % mapping_path
+
+        print '%s: Reading %s' % (filebase,mapping_path)
         df_mapping = pd.read_csv(mapping_path,sep='\t',header=0,index_col=0);
-
+       
     elif filebase in df_meta_plates:
-
-        print 'Found Meta-Data for %s in meta.txt' % filebase
+        
+        print '%s: Found meta-data in meta.txt' % (filebase),
 
         metadata = df_meta[df_meta.Plate_ID==filebase] # pd.DataFrame
 
@@ -148,26 +178,35 @@ for filename in list_data:
 
         if biolog:
             df_mapping = plates.expandBiologMetaData(metadata)
-            print 'and %s seems to be a BIOLOG plate' % filebase
+            print 'and seems to be a BIOLOG plate' 
         else:
             df_mapping = plates.initKeyFromMeta(metadata,well_ids)
-            print 'and %s does not seem to be a BIOLOG plate' % filebase
+            print 'and does not seem to be a BIOLOG plate'
 
     elif plates.isBIOLOG(filebase):
 
-        print filebase
         df_mapping = plates.initializeBiologPlateKey(filebase)
 
-        print 'Did not find file or meta-data but %s seems to be a BIOLOG plate' % filebase
+        print '%s: Did not find file or meta-data but seems to be a BIOLOG plate' % filebase
 
     else:
         df_mapping = pd.DataFrame(index=well_ids,columns=['Plate_ID'],
                                   data = [filebase]*len(well_ids))
         
-        print 'Did not find file or meta-data and %s does not seems to be a BIOLOG plate' % filebase
+        print '%s Did not find file or meta-data and does not seem to be a BIOLOG plate' % filebase
 
-    MAPPING[filebase] = df_mapping.reset_index(drop=False)
+    df_mapping = df_mapping.reset_index(drop=False)
 
+    return df_mapping
+
+df_meta, df_meta_plates = checkMetaTxt(FILES['META'])
+
+print 'READING & ASSEMBLING MAPPING'
+for filename in list_data:
+
+    filebase = os.path.splitext(filename)[0]
+    MAPPING[filebase] = smartMapping(filebase,DIRECTORY,DATA,df_meta,df_meta_plates)
+    
 df_mapping = pd.concat(MAPPING.values(),ignore_index=True,sort=False)
 df_mapping.to_csv('%s/stitched_mapping.txt' % DIRECTORY['MAPPING'],sep='\t',header=True,index=True)
 
